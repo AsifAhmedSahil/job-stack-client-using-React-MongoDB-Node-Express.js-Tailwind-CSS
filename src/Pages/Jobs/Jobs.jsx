66f3319e159a9397f7b8314923e097b6b1a3e685
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-29 02:28:42 +0600

    job description data setup

--- src/Pages/Jobs/Jobs.jsx
+++ src/Pages/Jobs/Jobs.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Jobs = ({result}) => {
   return (
-    <div>
-      {result}
+    <div className='bg-[#FAFAFA] md:grid grid-cols-4 md:px-24 px-4 py-12'>
+        <div className='bg-white p-4 rounded'>left</div>
+        <div className='bg-white p-4 rounded col-span-2'>{result}</div>
+        <div className='bg-white p-4 rounded'>right</div>
+      
       
     </div>
   )
--- src/assets/Components/Card.jsx
+++ src/assets/Components/Card.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { IoLocationOutline } from "react-icons/io5";
+import { FiCalendar, FiClock, FiDollarSign } from "react-icons/fi";
 
 const Card = ({data}) => {
+    const {companyName,jobTitle,companyLogo,minPrice,maxPrice,salaryType,jobLocation,postingDate,experienceLevel,employmentType,description} = data;
   return (
-    <div>
-      {/* {data} */}
-      <h3>{data.jobTitle }</h3>
-    </div>
+    <section className='card'>
+        <Link to={"/"} className='flex flex-col gap-4 sm:flex-row items-start'>
+        <img src={companyLogo} alt="" />
+        <div>
+            <h4 className='text-primary mb-1'>{companyName}</h4>
+            <h4 className='text-lg font-semibold mb-2'>{jobTitle}</h4>
+            <div className='text-primary/70 text-base flex flex-wrap gap-4 mb-2'>
+                <span className='flex items-center gap-2'><IoLocationOutline/>{jobLocation}</span>
+                <span className='flex items-center gap-2'><FiClock/>{employmentType}</span>
+                <span className='flex items-center gap-2'><FiDollarSign/>{minPrice}-{maxPrice}</span>
+                <span className='flex items-center gap-2'><FiCalendar/>{postingDate}</span>
+            </div>
+            <p className='text-primary/70 text-base'>{description}</p>
+        </div>
+        </Link>
+
+    </section>
   )
 }
 
