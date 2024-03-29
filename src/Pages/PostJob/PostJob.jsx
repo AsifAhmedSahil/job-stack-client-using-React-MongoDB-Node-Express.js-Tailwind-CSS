686148b306a57402454cfda9ae2de09dc46c43bb
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-29 18:03:57 +0600

    progress post job section

--- src/Pages/PostJob/PostJob.jsx
+++ src/Pages/PostJob/PostJob.jsx
@@ -18,9 +18,16 @@ const PostJob = () => {
       <div className="bg-[#FAFAFA] xl:px-16 px-4 xl:py-10 py-4 ">
         <form onSubmit={handleSubmit(onSubmit)}>
           
-          
+          <div className="flex flex-col lg:flex-row justify-between items-center gap-8">
+            <div className="lg:w-1/2 w-full">
+                <label  className="text-lg mb-2 block"> Job Title </label>
+                <input type="text" defaultValue={"web developer"} {...register("jobTitle")}  className="block w-full flex-1 border-1 py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6"/>
 
-          <input type="submit" />
+            </div>
+
+          </div>
+
+          <input type="submit" className="mt-5" />
         </form>
       </div>
     </div>
