7fc5d9385000aa72c6dbdfac5b66c5999a766665
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-31 03:43:50 +0600

    allignment

--- src/Pages/MyJobs/MyJobs.jsx
+++ src/Pages/MyJobs/MyJobs.jsx
@@ -16,8 +16,17 @@ const MyJobs = () => {
         })
     },[])
   return (
-    <div>
-      my jobs: {jobs.length}
+    <div className='max-w-screen-2xl container mx-auto lg:px-24 px-4'>
+
+      
+      <div>
+        <h1 className='text-center text-2xl mt-6 mb-4'>All My Jobs</h1>
+        <div className='text-center mb-4 mt-12'>
+          <input type="text " name='search' id='search' className='lg:w-6/12 w-full py-2 pl-3 border focus:outline-none  mb-4' />
+          <button className='bg-blue text-white py-2 px-8 mb-4 rounded-sm'>Search</button>
+        </div>
+
+      </div>
     </div>
   )
 }
