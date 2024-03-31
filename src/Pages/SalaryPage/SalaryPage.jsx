379cce8ef878f7988bca0d82343d05e4bd6e6a8d
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-04-01 03:13:47 +0600

    resolve issue mapping

--- src/Pages/SalaryPage/SalaryPage.jsx
+++ src/Pages/SalaryPage/SalaryPage.jsx
@@ -1,43 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import PageHeader from '../../assets/Components/PageHeader'
+import React, { useEffect, useState } from "react";
+import PageHeader from "../../assets/Components/PageHeader";
 
 const SalaryPage = () => {
-    const [searchText , setSearchText] = useState("")
-    const [salary , setSalary] = useState([])
-    console.log(searchText)
+  const [searchText, setSearchText] = useState("");
+  const [salary, setSalary] = useState([]);
+  console.log(searchText);
 
-    useEffect(() =>{
-        fetch("http://localhost:3000/salary")
-        .then(res => res.json())
-        .then((data) =>{
-            console.log(data)
-            setSalary(data)
-        })
-    },[])
+  useEffect(() => {
+    fetch("http://localhost:3000/salary")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setSalary(data);
+      });
+  }, [searchText]);
 
-    const handleSearch = () => {
-        const filter = salary.filter(
-          (job) =>
-            job.title.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
-        );
-        console.log(filter)
-        
-        setSalary(filter)
-      };
+  const handleSearch = () => {
+    const filter = salary.filter(
+      (job) => job.title.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+    );
+    console.log(filter);
 
+    setSalary(filter);
+  };
 
   return (
     <div className="max-w-screen-2xl container mx-auto lg:px-24 px-4">
-        <PageHeader title={"Estimated Salary"}/>
+      <PageHeader title={"Estimated Salary"} />
 
-        <div className='mt-5'>
+      <div className="mt-5">
         <div className="text-center mb-4 mt-12">
           <input
             onChange={(e) => setSearchText(e.target.value)}
             type="text "
             name="search"
             id="search"
-            
             className="lg:w-6/12 w-full py-2 pl-3 border focus:outline-none  mb-4"
           />
           <button
@@ -47,11 +44,19 @@ const SalaryPage = () => {
             Search
           </button>
         </div>
+      </div>
 
-        </div>
-      
+      {/* salary display card */}
+      <div className="text-black">
+        {salary.map((data) => (
+          <div key={data._id}>
+            <h4>{data.title}</h4>
+            <p>{data.salary}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SalaryPage
+export default SalaryPage;
