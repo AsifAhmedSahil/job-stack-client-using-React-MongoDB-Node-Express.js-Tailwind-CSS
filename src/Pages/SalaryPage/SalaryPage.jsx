5dcb5d068d63b577ac9c5af7eeace69a40a087ab
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-31 19:17:04 +0600

    search design

--- src/Pages/SalaryPage/SalaryPage.jsx
+++ src/Pages/SalaryPage/SalaryPage.jsx
@@ -5,6 +5,25 @@ const SalaryPage = () => {
   return (
     <div className="max-w-screen-2xl container mx-auto lg:px-24 px-4">
         <PageHeader title={"Estimated Salary"}/>
+
+        <div className='mt-5'>
+        <div className="text-center mb-4 mt-12">
+          <input
+            onChange={(e) => setSearchText(e.target.value)}
+            type="text "
+            name="search"
+            id="search"
+            className="lg:w-6/12 w-full py-2 pl-3 border focus:outline-none  mb-4"
+          />
+          <button
+            className="bg-blue text-white py-2 px-8 mb-4 rounded-sm"
+            // onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
+
+        </div>
       
     </div>
   )
