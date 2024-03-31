faebe3b47b8886bbc25e0ea0bae1420e727087b4
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-31 07:27:27 +0600

    add table and show data

--- src/Pages/MyJobs/MyJobs.jsx
+++ src/Pages/MyJobs/MyJobs.jsx
@@ -1,34 +1,152 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FaEdit } from "react-icons/fa";
+import { FaTrashAlt } from "react-icons/fa";
 
 const MyJobs = () => {
-    const email = "sahil@gmail.com"
-    const [isLoading,setISLoading] = useState(true);
-    const [searchText,setSearchText] = useState("")
-    const [jobs,setJobs] = useState([])
+  const email = "sahil@gmail.com";
+  const [isLoading, setISLoading] = useState(true);
+  const [searchText, setSearchText] = useState("");
+  const [jobs, setJobs] = useState([]);
 
-    useEffect(() =>{
-        setISLoading(true)
-        fetch(`http://localhost:3000/myJobs/${email}`)
-        .then((res) => res.json())
-        .then((data ) =>{
-            console.log(data)
-            setJobs(data);
-        })
-    },[])
+  useEffect(() => {
+    setISLoading(true);
+    fetch(`http://localhost:3000/myJobs/${email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setJobs(data);
+      });
+  }, []);
+
+  const handleSearch = () => {
+    const filter = jobs.filter(
+      (job) =>
+        job.jobTitle.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+    );
+    // console.log(filter)
+    setJobs(filter);
+    setISLoading(false);
+  };
+
+  const handleDelete = (id) =>{
+    
+  }
   return (
-    <div className='max-w-screen-2xl container mx-auto lg:px-24 px-4'>
-
-      
+    <div className="max-w-screen-2xl container mx-auto lg:px-24 px-4">
       <div>
-        <h1 className='text-center text-2xl mt-6 mb-4'>All My Jobs</h1>
-        <div className='text-center mb-4 mt-12'>
-          <input type="text " name='search' id='search' className='lg:w-6/12 w-full py-2 pl-3 border focus:outline-none  mb-4' />
-          <button className='bg-blue text-white py-2 px-8 mb-4 rounded-sm'>Search</button>
+        <h1 className="text-center text-2xl mt-6 mb-4">All My Jobs</h1>
+        <div className="text-center mb-4 mt-12">
+          <input
+            onChange={(e) => setSearchText(e.target.value)}
+            type="text "
+            name="search"
+            id="search"
+            className="lg:w-6/12 w-full py-2 pl-3 border focus:outline-none  mb-4"
+          />
+          <button
+            className="bg-blue text-white py-2 px-8 mb-4 rounded-sm"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
         </div>
 
+        <div>
+          <section className="py-1 bg-blueGray-50">
+            <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-8">
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+                <div className="rounded-t mb-0 px-4 py-3 border-0">
+                  <div className="flex flex-wrap items-center">
+                    <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+                      <h3 className="font-semibold text-base text-blueGray-700">
+                        All Jobs
+                      </h3>
+                    </div>
+                    <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+                      <Link to='/post-job'>
+                      <button
+                        className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button"
+                      >
+                        Post a job
+                      </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="block w-full overflow-x-auto">
+                  <table className="items-center bg-transparent w-full border-collapse ">
+                    <thead>
+                      <tr>
+                        <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                          No.
+                        </th>
+                        <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                         Title
+                        </th>
+                        <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                        Company Name
+                        </th>
+                        <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                          Salary
+                        </th>
+                        <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                          Edit
+                        </th>
+                        <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                          Delete
+                        </th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      {
+                        jobs.map((job,index) =>(
+                          <tr key={index}>
+                        <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                          {index+1}
+                        </th>
+                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                          {job.jobTitle}
+                        </td>
+                        <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                          {job.companyName}
+                        </td>
+                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                          <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
+                          $ {job.minPrice}k - $ {job.maxPrice}k
+                        </td>
+                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                          <button className=""><Link to={`/edit-job/${job._id}`}><FaEdit size={20} className="text-blue"></FaEdit></Link></button>
+                        </td>
+                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <Link onClick={() => handleDelete(job._id)}><FaTrashAlt size={20} className="text-red-600"></FaTrashAlt></Link>
+                        </td>
+                      </tr>
+                        )
+
+                          
+                          
+                          
+
+
+                        )
+                      }
+                      
+                      
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          
+          </section>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyJobs
+export default MyJobs;
