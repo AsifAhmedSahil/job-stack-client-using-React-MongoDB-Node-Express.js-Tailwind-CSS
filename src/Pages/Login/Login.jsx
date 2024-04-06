3c5e0287c224b129138d386892f5108a519bcd5d
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-04-06 19:52:23 +0600

    stack

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='h-screen w-full flex items-center justify-center '>
-      <h1 className='px-4 py-2 bg-blue text-white rounded'>Login</h1>
+      {/* <h1 className='px-4 py-2 bg-blue text-white rounded'>Login</h1> */}
     </div>
   )
 }
